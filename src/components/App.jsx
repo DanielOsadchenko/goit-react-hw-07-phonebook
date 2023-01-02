ccad8c8499ca70ff911794d5cf33775471b5d873
DanielOsadchenko <dm.osadchenko@gmail.com>
2023-01-02 11:54:57 +0100

    task complited

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
+import ContactForm from './ContactForm/ContactForm';
+import { Contacts } from './Contacts/Contacts';
+import Filter from './Filter/Filter';
+import { useState } from 'react';
+export default function App() {
+  const [filter, setFilter] = useState('');
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
-    >
-      React homework template
+      >
+      <h1>Phonebook</h1>
+      <ContactForm/>
+
+      <h2>Contacts</h2>
+      <Filter saveFilter={setFilter} />
+      <Contacts filter={filter} />
+
     </div>
   );
-};
+}
+
